0e349c50696e3505bf61117da3bdb4edd99de9ff
sardinhas12 <patriciasardinharocha@gmail.com>
2022-07-07 15:21:37 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,19 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+var pronoun = ["the", "our"];
+var adj = ["great", "big"];
+var noun = ["jogger", "racon"];
+var result = "";
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        result = pronoun[i] + adj[j] + noun[k] + ".com";
+        document.querySelector.innerHTML = result;
+        console.log(pronoun[i] + adj[j] + noun[k] + ".com");
+      }
+    }
+  }
 };
